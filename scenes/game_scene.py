69b707f9dd6c1ed1d1996f5dd3a67f97d0a062bc
sof00chka <sprende@mail.ru>
2026-01-19 20:56:00 +0300

    текст исправлен

--- scenes/game_scene.py
+++ scenes/game_scene.py
@@ -4,9 +4,9 @@ from scenes.base_scene import BaseScene
 
 
 class GameScene(BaseScene):
-    def on_draw(self):
-        self.clear()
-        arcade.draw_text(
+    def __init__(self, window):
+        super().__init__(window)
+        self.text = arcade.Text(
             "GAME SCENE\nESC - Back to menu",
             400,
             300,
@@ -15,6 +15,10 @@ class GameScene(BaseScene):
             anchor_x="center",
             anchor_y="center"
         )
+
+    def on_draw(self):
+        self.clear()
+        self.text.draw()
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.ESCAPE:
