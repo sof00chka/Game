8d93c01cd0b495e1746203ad3100907166f3cc58
sof00chka <sprende@mail.ru>
2026-01-20 03:18:10 +0300

    камера

--- scenes/game_scene.py
+++ scenes/game_scene.py
@@ -1,10 +1,11 @@
 import arcade
-
 from scenes.base_scene import BaseScene
 from objects.player import Player
 from objects.wall import Wall
-
-from core.constants import PLAYER_SPEED, SCREEN_WIDTH, SCREEN_HEIGHT
+from core.constants import (
+    PLAYER_SPEED, SCREEN_WIDTH, SCREEN_HEIGHT,
+    WORLD_WIDTH, WORLD_HEIGHT, CAMERA_LERP
+)
 
 
 class GameScene(BaseScene):
@@ -30,9 +31,15 @@ class GameScene(BaseScene):
         #     self.wall_list.append(wall)
         #     self.all_sprites.append(wall)
 
+        self.world_camera = arcade.camera.Camera2D()
+        self.gui_camera = arcade.camera.Camera2D()
+        self.world_camera.position = (self.player.center_x, self.player.center_y)
+
     def on_draw(self):
         self.clear()
+        self.world_camera.use()
         self.all_sprites.draw()
+        self.gui_camera.use()
         self.text.draw()
 
     def on_update(self, delta_time: float):
@@ -43,11 +50,22 @@ class GameScene(BaseScene):
         if hit_list:
             self.player.center_x = x
             self.player.center_y = y
+        cam_x, cam_y = self.world_camera.position
+        player_x, player_y = self.player.center_x, self.player.center_y
+
+        smooth_x = cam_x + (player_x - cam_x) * CAMERA_LERP
+        smooth_y = cam_y + (player_y - cam_y) * CAMERA_LERP
+
+        half_w = SCREEN_WIDTH // 2
+        half_h = SCREEN_HEIGHT // 2
+        smooth_x = max(half_w, min(WORLD_WIDTH - half_w, smooth_x))
+        smooth_y = max(half_h, min(WORLD_HEIGHT - half_h, smooth_y))
+
+        self.world_camera.position = (smooth_x, smooth_y)
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.ESCAPE:
             self.window.show_menu()
-
         elif key == arcade.key.UP:
             self.player.change_y = PLAYER_SPEED
         elif key == arcade.key.DOWN:
