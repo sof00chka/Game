934059128a92c27103c9aaddde635c70b8485c8d
sof00chka <sprende@mail.ru>
2026-01-21 20:42:32 +0300

    .

---
+++ resources/player.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="player" tilewidth="105" tileheight="120" tilecount="1" columns="1">
+ <image source="../PycharmProjects/Game/resources/player.png" width="105" height="120"/>
+</tileset>
