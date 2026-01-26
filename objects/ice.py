925dfd06309286e412d2479a233dbe75b7068f95
sof00chka <sprende@mail.ru>
2026-01-26 18:31:07 +0300

    графика

--- objects/ice.py
+++ objects/ice.py
@@ -3,7 +3,7 @@ import arcade
 
 class Ice(arcade.Sprite):
     def __init__(self, x, y, scale=1):
-        super().__init__("resources/ice.png", scale)
+        super().__init__(scale)
 
         self.center_x = x
         self.center_y = y
